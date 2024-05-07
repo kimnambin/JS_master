a4e49e30cbfa5a59a251107c6bdc97ee03f38e91
kimnambin <mkkim044@naver.com>
2024-05-07 16:19:44 +0900

    이미지 애니메이션 추가

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ViewChildren, QueryList, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements AfterViewInit {
+  @ViewChildren('aniItem')
+  aniItems!: QueryList<any>;
+  currentIndex: number = 0;
+  totalItems: number = 0;
 
-  constructor() {}
+  constructor(private renderer: Renderer2) {}
 
+  ngAfterViewInit() {
+    this.totalItems = this.aniItems.length;
+    setInterval(() => this.rotateImages(), 2000); // 이미지를 변경하는 간격 (밀리초)
+  }
+
+  rotateImages() {
+    if (this.aniItems) {
+      const itemsArray = this.aniItems.toArray();
+      if (itemsArray[this.currentIndex]) {
+        this.renderer.setStyle(itemsArray[this.currentIndex].nativeElement, 'transform', 'translateX(-100%)');
+      }
+      const nextIndex = (this.currentIndex + 1) % this.totalItems;
+      if (itemsArray[nextIndex]) {
+        this.renderer.setStyle(itemsArray[nextIndex].nativeElement, 'transform', 'translateX(0)');
+      }
+      this.currentIndex = nextIndex;
+    }
+  }
 }
