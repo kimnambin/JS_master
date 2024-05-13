b157751ec35db37583f6cd7178be2a240dceb5e4
kimnambin <mkkim044@naver.com>
2024-05-13 15:49:44 +0900

    메인 페이지 부분 구현 완료 - section부분은 수정해야함 ㅠㅠ

--- src/app/study01/study01.module.ts
+++ src/app/study01/study01.module.ts
@@ -8,12 +8,13 @@ import { Study01PageRoutingModule } from './study01-routing.module';
 
 import { Study01Page } from './study01.page';
 
+
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    Study01PageRoutingModule
+    Study01PageRoutingModule,
   ],
   declarations: [Study01Page]
 })
